ce57b9640fe8bc646e1476449cc6590d6b3c3526
Anikó Lengyel <lengyelaniko93@gmail.com>
2018-06-22 20:17:01 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -260,91 +260,113 @@ var locations = [
     }
   ]
 
+
+var markers = [];
+var map;
+var markers = [];
+
+var fsClientID = "5EX5WDIQSCH5T0FF44WIBB35B1URTUCF0LKDVK5FBNJPJ5FO";
+var fsClientSecret = "KFPB0A0BS5OVVKKBZKCF35YD5OG3FYPQQ1O3MCMQPAGWKSVP";
+
+
 var ViewModel = function() {
   var self = this;
 
   this.places = ko.observableArray(locations);
-  // getting the client ids and secret for the FouSquare api
 
-  var fsClientID = "5EX5WDIQSCH5T0FF44WIBB35B1URTUCF0LKDVK5FBNJPJ5FO";
-  var fsClientSecret = "KFPB0A0BS5OVVKKBZKCF35YD5OG3FYPQQ1O3MCMQPAGWKSVP"
-  var markers = [];
-  var addressArray = [];
-
-  var map;
-
-  initMap = function() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom:13,
-      center: {lat: 47.4970, lng: 19.0634},
-      styles: styles
-    });
-    console.log("the map is ready!");
-
-    //creating infowindow
-
-    for (var i = 0; i < locations.length; i++) {
-      // Get the position from the location array.
-      var position = locations[i].location;
-      var title = locations[i].name;
-      // Create a marker per location, and put into markers array.
-      var marker = new google.maps.Marker({
-        position: position,
-        map: map,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        //icon: defIcon,
-        id: i
-      });
-      markers.push(marker);
-
-      // get the fs info from fs api - per marker.position, marker.title
-      // set infowindow content to title + foursquare content
-
-      // creating a url string for every request
-
-      // remove and change characters to the request url String
-      var markerPosition = String(marker.position);
-      var markerTitle = String(marker.title);
-
-      var markerPosition = markerPosition.replace('(','');
-      var markerPosition = markerPosition.replace(')','');
-      var markerPosition = markerPosition.replace('%','');
-      var markerTitle = markerTitle.split(' ').join('+');
-
-      console.log(markerTitle);
-
-      var urlString = "https://api.foursquare.com/v2/venues/search?ll=" +
-      markerPosition + "&name=" + markerTitle + "&intent=match&client_id="
-      + fsClientID + "&client_secret=" + fsClientSecret + "&v=20180622";
-
-      console.log(urlString);
-
-      $.getJSON(urlString, function(data) {
-        var markerInfo = data.response.venues[0].location.formattedAddress
-        //console.log(markerInfo);
-        addressArray.push(markerInfo);
-        console.log(addressArray);
-        return markerInfo;
-      });
-
-      //populateInfowindow(map, marker, markerInfo);
-
-    }
-
-    console.log("the markers are ready!")
 };
 
-  populateInfowindow = function(map, marker, contentInfo) {
-    var infowindow = new google.maps.InfoWindow({
-      content: contentInfo
+initMap = function() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom:13,
+    center: {lat: 47.4970, lng: 19.0634},
+    styles: styles
+  });
+  console.log("the map is ready!");
+
+  for (var i = 0; i < locations.length; i++) {
+    // Get the position from the location array.
+    var position = locations[i].location;
+    var title = locations[i].name;
+    // Create a marker per location, and put into markers array.
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
     });
+    markers.push(marker);
+    //console.log(markers);
+  }
+  var addressArray = getFsInfo();
+  console.log(addressArray);
+  populateInfowindow = function(addressArray, map, marker) {
+    for(var i = 0; i < addressArray.length; i++) {
+      var markerInfo = addressArray[i];
+      console.log("markerInfo: ", markerInfo);
+      var infowindow = new google.maps.InfoWindow({
+        content: markerInfo
+      });
+      console.log("marker: ", marker);
+      marker.addListener('click', function () {
+        infowindow.open(map, marker);
+      })
+    }
+    console.log('We created the markers with listeners!');
+  }
+  populateInfowindow();
+
+}
+
+getFsInfo = function() {
+  var addressArray = [];
+
+  for(var i = 0; i<markers.length; i++) {
+    var markerPosition = String(markers[i].position);
+    var markerTitle = String(markers[i].title);
+    var markerPosition = markerPosition.replace('(','');
+    var markerPosition = markerPosition.replace(')','');
+    var markerPosition = markerPosition.replace('%','');
+    var markerPosition = markerPosition.replace(' ','');
+    //console.log(markerPosition);
+    var markerTitle = markerTitle.split(' ').join('+');
+    //console.log(markerTitle);
+
+    var urlString = "https://api.foursquare.com/v2/venues/search?ll="
+    + markerPosition + "&name="
+    + markerTitle + "&intent=match&client_id="
+    + fsClientID + "&client_secret=" + fsClientSecret + "&v=20180622";
+
+    //console.log(urlString);
+
+    $.getJSON(urlString, function(data) {
+      var markerInfo = data.response.venues[0].location.formattedAddress
+      //console.log(markerInfo);
+      addressArray.push(markerInfo);
+      console.log(addressArray);
+    });
+  }
+  console.log("type: ", typeof addressArray);
+  return addressArray;
+}
+
+populateInfowindow = function(infoArray, map, marker) {
+  for(var i = 0; i<infoArray.length; i++) {
+    var markerInfo = infoArray[i];
+    console.log("markerInfo: ", markerInfo);
+    var infowindow = new google.maps.InfoWindow({
+      content: markerInfo
+    });
+    console.log("marker: ", marker);
     marker.addListener('click', function () {
       infowindow.open(map, marker);
     })
   }
+  console.log('We created the markers with listeners!');
 
 }
+
 
 $(document).ready(function(){
   ko.applyBindings(new ViewModel());
