6cbfe575ac9b1be0d21e60d4c3be87d4700443e7
Anikó Lengyel <lengyelaniko93@gmail.com>
2018-06-25 18:49:12 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-//foursquare API keys
+//define API keys
 var fsClientID = "5EX5WDIQSCH5T0FF44WIBB35B1URTUCF0LKDVK5FBNJPJ5FO";
 var fsClientSecret = "KFPB0A0BS5OVVKKBZKCF35YD5OG3FYPQQ1O3MCMQPAGWKSVP";
-var oauth_token = "I4AOIKFPDBTV1JHUEUTRKGJ0SW4ONTOMEBNPXGXEPOT0ZSQZ";
 
-//define map as global variable
+//define map
 var map;
 
-//Global ViewModel object
+//creating the viewModel object
 var ViewModel = function() {
   // differrentiate between ViewModel and "local" object
   self = this;
-  // create array object
+  // create an array to store the markers
   self.markers = new Array();
+  // creting an observable to bind the user's search query
   self.search = ko.observable('');
 
-  //initialize map
+  //initialize map woth a darkblue style
+  // the theme is from https://mapstyle.withgoogle.com/ - Aubergine style
   this.initMap = function() {
     console.log("Creating map...");
     map = new google.maps.Map(document.getElementById('map'), {
@@ -24,24 +25,31 @@ var ViewModel = function() {
     });
   }
 
-  // populator function to bind event listeners, fetch information and animates marker
+  // populator function to bind event listeners, fetch information and animate markers
   this.populateMarker = function(){
+    // set the animation to bounce
     this.setAnimation(google.maps.Animation.BOUNCE);
+    // change the icon color to red for the selected marker
     this.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
     setTimeout((function() {
+        // after timeout, change the color back to blue
         this.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png');
+        // stop the animation
         this.setAnimation(null);
     }).bind(this), 1200);
+    // get the place info for the marker
     self.fetchInfo(this);
   };
 
-  // create ViewModel infoWindow
+  // create infoWindow for the markers
   this.createInfoWindow = function() {
       console.log("Creating infowindow...");
       self.infoWindow = new google.maps.InfoWindow();
   };
 
-  this.createMarkers = function() {
+  // get the data for markers from the model (locations)
+  this.createMarkerData = function() {
+    // creating an array to store the markerd data objects
     var markerData = [];
 
     console.log("Creating marker data from locations...");
@@ -52,9 +60,11 @@ var ViewModel = function() {
       obj.lat = locations[i].location.lat;
       obj.lng = locations[i].location.lng;
 
+      // append the marker data objects to the array
       markerData.push(obj);
     }
     console.log("Created markers: ", markerData);
+    // create marker based on the marker data
     self.createMapsMarkers(markerData);
   };
 
@@ -72,11 +82,15 @@ var ViewModel = function() {
           animation: google.maps.Animation.DROP
         });
         this.marker.setMap(map);
+        // add an eventListener to populate the map for user click
         this.marker.addListener('click', self.populateMarker);
+        // appending the marker to the markers array
         this.markers.push(this.marker);
       }
   };
 
+  // get Foursquare info for the infowindow
+  // we need the address and the rating
   this.fetchInfo = function(marker) {
     console.log("Event listener ", marker.title);
     var markerInfo;
@@ -85,8 +99,7 @@ var ViewModel = function() {
     var placename;
     var location;
 
-
-    //GET API request builder
+    //build the API request to get the location and venue_id
     var searchRequest = "https://api.foursquare.com/v2/venues/search?ll="
     + String(marker.position).replace(pattern, '') + "&name="
     + String(marker.title).split(' ').join('+') + "&intent=match&client_id="
@@ -100,19 +113,22 @@ var ViewModel = function() {
         address = markerInfo.location.address;
         var venue_id = markerInfo.id;
 
+        // with the venue_id, build another request string to get the rating
         var ratingRequest = "https://api.foursquare.com/v2/venues/" + String(venue_id) +
         "?client_id=" + fsClientID + "&client_secret=" + fsClientSecret + "&v=20180622";
 
         $.getJSON(ratingRequest).done(function(data) {
           try {
             venueRating = data.response.venue.rating;
-            
+
+            // after the requests are done, set the content for the infowindow
             self.infoWindow.marker = marker;
             self.infoWindow.setContent("<h2>" + placename + "</h2>" + "<h2>" + address + "</h2>" + "<h2> Rating: " + venueRating + "</h2>");
             self.infoWindow.open(map, marker);
           }
+          // handling the exceptions
           catch (e) {
-            console.log("No results for ", marker.title, e);
+            console.log("No information for ", marker.title, e);
           }
         });
       }
@@ -120,7 +136,7 @@ var ViewModel = function() {
         console.log("No results for ", marker.title, e);
       }
     }).fail(function() {
-      console.log("API error at ", marker.title);
+      console.log("API error here: ", marker.title);
     });
   };
 
@@ -129,32 +145,38 @@ var ViewModel = function() {
   //create infoWindow object
   this.createInfoWindow();
   //create marker data and Google Maps markers
-  this.createMarkers();
+  this.createMarkerData();
 
+
+  // a function to get the user results and show the markers
   this.venues = ko.computed(function() {
     console.log("Update ", self.markers.length," elements");
-    // results is for filtering
-    var results = [];
+    // array to store the search results
+    var filteredResults = [];
     console.log("Filtering for ", self.search());
     for (var i = 0; i < self.markers.length; i++){
+      // if the search query contains the marker title, append the title
+      // to the results
         if (self.markers[i].title.toLowerCase().includes(self.search().toLowerCase())){
-          results.push(self.markers[i]);
+          filteredResults.push(self.markers[i]);
+          // set visible the particular markers
           self.markers[i].setVisible(true);
         } else {
           self.markers[i].setVisible(false);
         };
         //todo: sort the titles by abc
     }
-    console.log("Findings: ", results);
-    results.sort(compare);
-    console.log("sorted results: ", results);
-    return results;
+    console.log("Findings: ", filteredResults);
+    // sort the results by abc
+    filteredResults.sort(compare);
+    console.log("sorted results: ", filteredResults);
+    return filteredResults;
   }, this);
 
 
 };
 
-// a function to sorting the titles by abc
+// a helper function to sorting the titles by abc
 function compare(a,b) {
   if(a.title < b.title) {
     return -1;
@@ -165,7 +187,7 @@ function compare(a,b) {
   return 0;
 }
 
-
+// when the html is downloaded, apply the bindings
 $(document).ready(function(){
   ko.applyBindings(new ViewModel());
 });
--- model.js
+++ model.js
@@ -2,33 +2,27 @@
 var locations = [
     {
       location: {"lat": 47.4970, "lng": 19.0634},
-      "name": "Szimpla Kert",
-      "place_id": "ChIJbxDkwWfcQUcRoaZDJzYZqyE"
+      "name": "Szimpla Kert"
     },
     {
       location: {"lat": 47.501341, "lng": 19.06528},
-      "name": "Instant",
-      "place_id": "ChIJ3RbjiB3cQUcRMR6GqCXvzzk"
+      "name": "Instant"
     },
     {
       location: {"lat" : 47.500737, "lng" : 19.059082},
-      "name": "Aker't",
-      "place_id": "ChIJeUfBw2vcQUcRsBNLJjNNazA"
+      "name": "Aker't"
     },
     {
       location: {"lat" : 47.5005917, "lng" : 19.0692819},
-      "name": "Fogas haz",
-      "place_id": "ChIJSXAClWjcQUcRGx30AEH0nZY"
+      "name": "Fogas haz"
     },
     {
       location: {"lat" : 47.4960738, "lng" : 19.0654356},
-      "name": "Corvin Club & Roof Terrace",
-      "place_id": "ChIJ13TLfGfcQUcRaUEdSMYgIeg"
+      "name": "Corvin Club & Roof Terrace"
     },
       {
         location: {"lat" : 47.4984, "lng" : 19.0543},
-        "name": "Akvárium Klub",
-        "place_id": "ChIJ13TLfGfcQUcRaUEdSMYgIeg"
+        "name": "Akvárium Klub"
       },
       {
         location: {"lat" : 47.4981634, "lng" : 19.066474200000016},
